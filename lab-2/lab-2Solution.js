fdede503b36ac3fd6270e48e7b21ca5811f4bb02
hsujessie <j986608@gmail.com>
2023-01-24 15:14:25 -0800

    added lab-2Solution.js

---
+++ lab-2/lab-2Solution.js
@@ -0,0 +1,14 @@
+function countTrue(arr) {
+    let count = 0;
+    for(let i = 0; i < arr.length; i ++) {
+        if(arr[i]) {
+            count++;
+        }
+    }
+
+    console.log(count);
+}
+
+countTrue([true, false, false, true, false]);
+countTrue([false, false, false, false]);
+countTrue([]);
