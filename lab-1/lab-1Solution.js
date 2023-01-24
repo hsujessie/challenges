9f4083ac10a5d5c1e802c0aed7cf4f030bec461d
hsujessie <j986608@gmail.com>
2023-01-24 15:56:59 -0800

    added Teacher's solution in lab-1Solution.js

--- lab-1/lab-1Solution.js
+++ lab-1/lab-1Solution.js
@@ -1,3 +1,5 @@
+//My solution
+/*
 function questionOne() {
     let arr = [];
     for(let j = 1; j < 101; j++) {
@@ -17,7 +19,7 @@ function questionOne() {
     console.log("Q1: " + arr);
 }
 questionOne();
-
+*/
 function questionTwo() {
     let sum = 0;
     for(let i = 1; i < 101; i++) {
@@ -41,3 +43,20 @@ function questionThree(arr) {
     return num;
 }
 console.log("Q3: " + questionThree([50,300,32,55,900,2,-1]));
+
+
+//Teacher's solution
+function callFizzBuzz() {
+    for(let i = 0; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if(i % 3 === 0) {
+            console.log("Fizz");
+        } else if(i % 5 === 0){
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+callFizzBuzz();
